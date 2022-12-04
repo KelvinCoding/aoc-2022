23ca918be601dcdd5bbb2cd957e463f866fb24ba
Kelvin Lee <me@kelvincoding.com>
2022-12-03 23:15:14 -0600

    feat: day04 solution

---
+++ src/day04/solve.test.ts
@@ -0,0 +1,90 @@
+import { beforeAll, describe, it, expect } from 'vitest';
+import fs from 'fs';
+import path from 'path';
+
+function solve1(lines: string[]) {
+  return lines.reduce((acc, line) => {
+    if (!line) {
+      return acc;
+    }
+
+    const [[aStart, aEnd], [bStart, bEnd]] = line
+      .split(',')
+      .map((str) => str.split('-').map((s) => parseInt(s, 10)));
+
+    const isBWithinA = aStart <= bStart && bEnd <= aEnd;
+    const isAWithinB = bStart <= aStart && aEnd <= bEnd;
+
+    if (isBWithinA || isAWithinB) {
+      return acc + 1;
+    }
+
+    return acc;
+  }, 0);
+}
+
+function solve2(lines: string[]) {
+  return lines.reduce((acc, line) => {
+    if (!line) {
+      return acc;
+    }
+
+    const [[aStart, aEnd], [bStart, bEnd]] = line
+      .split(',')
+      .map((str) => str.split('-').map((s) => parseInt(s, 10)));
+
+    const isBIntersectingA =
+      (aStart <= bStart || bEnd <= aEnd) && !(bEnd < aStart || bStart > aEnd);
+    const isAIntersectingB =
+      (bStart <= aStart || aEnd <= bEnd) && !(aEnd < bStart || aStart > bEnd);
+
+    if (isBIntersectingA || isAIntersectingB) {
+      return acc + 1;
+    }
+
+    return acc;
+  }, 0);
+}
+
+describe.only('day 04', () => {
+  describe('test input', () => {
+    let lines: string[];
+
+    beforeAll(() => {
+      const input = fs.readFileSync(
+        path.join(__dirname, 'input-test.txt'),
+        'utf-8'
+      );
+      lines = input.split('\n');
+    });
+
+    it('solves the first problem', () => {
+      const top = solve1(lines);
+      expect(top).toBe(2);
+    });
+
+    it('solves the second problem', () => {
+      const sum = solve2(lines);
+      expect(sum).toBe(4);
+    });
+  });
+
+  describe('actual input', () => {
+    let lines: string[];
+
+    beforeAll(() => {
+      const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf-8');
+      lines = input.split('\n');
+    });
+
+    it('solves the first problem', () => {
+      const top = solve1(lines);
+      console.log('First solution:', top);
+    });
+
+    it('solves the second problem', () => {
+      const sum = solve2(lines);
+      console.log('Second solution:', sum);
+    });
+  });
+});
